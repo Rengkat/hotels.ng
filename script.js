75cee82cbd982415ae469f6e71c82c68b7b13f7f
Rengkat <successinmotion11@gmail.com>
2023-04-06 23:58:41 +0100

    javascript added

---
+++ script.js
@@ -0,0 +1,31 @@
+// html elements
+const contact = document.querySelector(".call-us");
+const contactDropdown = document.querySelector(".dropdown-container");
+const currency = document.querySelector(".currency-top-info");
+const currencyDropdown = document.querySelector(".currency-dropdown-container");
+const profile = document.querySelector(".profile-info");
+const profileDropdown = document.querySelector(".profile-dropdown");
+// contact dropdown
+contact.addEventListener("click", () => {
+  if (contactDropdown.style.display == "none") {
+    contactDropdown.style.display = "block";
+  } else {
+    contactDropdown.style.display = "none";
+  }
+});
+// currency dropdown
+currency.addEventListener("click", () => {
+  if (currencyDropdown.style.display == "none") {
+    currencyDropdown.style.display = "block";
+  } else {
+    currencyDropdown.style.display = "none";
+  }
+});
+// profile dropdown
+profile.addEventListener("click", () => {
+  if (profileDropdown.style.display == "none") {
+    profileDropdown.style.display = "block";
+  } else {
+    profileDropdown.style.display = "none";
+  }
+});
